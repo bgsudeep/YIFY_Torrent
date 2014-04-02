053a6d335a96815efb417f799369dfea5e895229
Sudeep Bagale <bgsudeep@gmail.com>
2014-04-02 16:41:35 +0545

    android-async-http library added

--- src/com/bg/yify_torrents/MainActivity.java
+++ src/com/bg/yify_torrents/MainActivity.java
@@ -3,6 +3,7 @@ package com.bg.yify_torrents;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.loopj.android.http.*;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("http://www.google.com", new AsyncHttpResponseHandler() {
+		    @Override
+		    public void onSuccess(String response) {
+		        System.out.println(response);
+		    }
+		});
+		
 	}
 
 	@Override
